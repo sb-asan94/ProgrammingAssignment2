ff7b1c3a0e88f0eaa122121558beb0fa3a7a9d4c
sb-asan94 <49880889+sb-asan94@users.noreply.github.com>
2019-04-23 00:29:42 +0530

    Update cachematrix.R
    Updated R file as per instructions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+##set_mat- sets matrix
+##get_mat-gets the matrix
+##get_inverse- gets inverse of matrix
 
-## Write a short comment describing this function
+## Function: makeCacheMatrix- creates special matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<-function(x=matrix()){	
+inv<-NULL
 
+set_mat<-function(matrix){			#sets matrix
+x<<-matrix
+return(x)
+}
+
+get_mat<-function()x				#gets the matrix
+
+set_inverse<-function(){			#sets inverse of matrix
+inv<<-solve(x)
+return(inv)
+}
+	
+get_inverse<-function()inv			#gets inverse of matrix
+
+list(set_mat=set_mat,get_mat=get_mat,set_inverse=set_inverse,get_inverse=get_inverse)
 }
 
 
-## Write a short comment describing this function
+## Function: cacheSolve- creates inverse of special matrix returned by makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){			
+inv=x$get_inverse()
+if(!(is.null(inv))){
+print('getting cached data')
+return(inv)				#returns inverse from cache
+}else{
+mat<<-x$get_mat()
+inv_mat<<-x$set_inverse()
+#x$get_inverse(inv_mat)
+inv_mat				#returns inverse, if not previously calculated
 }
+}
+
+cacheSolve(makeCacheMatrix(matrix(4:7, 2)))
+cacheSolve(makeCacheMatrix(matrix(14:17,2)))
+
